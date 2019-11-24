9ed3739b7ffaa42d94a48f485bc66db85794371c
Denis M. Gabaidulin <gabaden@gmail.com>
2019-11-24 21:41:33 +0300

    * highlight current quarter

--- functions.R
+++ functions.R
@@ -9,20 +9,23 @@ numberFormatter <- function(x) {
   format(x, big.mark = ".")
 }
 
-revenuePlot <- function(fileName) {
-  data <- read.csv(fileName, sep = ",")
+isCurrentQuarter <- function(x, current) {
+  x %in% current
+}
+
+revenuePlot <- function(df) {
+  last <- length(df$Dt)
+  current <- c(as.character(df$Dt[last]), as.character(df$Dt[last - 4]))
   
-  # avoid sorting
-  data$Dt <- factor(data$Dt, levels = data$Dt)
-  data
-  
-  df <- data
+  isHighlighted <- function(x) {
+    isCurrentQuarter(x, current)
+  }
   
   breaks <- pretty(melt(df, id = "Dt")$value, n = 20)
   
   ggplot(data = df) +
     geom_bar(stat = "identity", aes(x = Dt, y = Revenue, fill = "Revenue"), col = I("black"), position = "dodge") +
-    geom_bar(stat = "identity", aes(x = Dt, y = Net.Profit, fill = "Net.Profit"), col = I("black"), position = "dodge") +
+    geom_bar(stat = "identity", aes(x = Dt, y = Net.Profit, fill = factor(ifelse(isHighlighted(Dt), "Net.Profit.Last", "Net.Profit"))), col = I("black"), position = "dodge") +
     theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, color = "#000000")) +
     theme(axis.text.y = element_text(color = "#000000")) +
     scale_y_continuous(breaks = breaks, labels = numberFormatter) +
